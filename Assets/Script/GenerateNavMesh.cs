19395b54c7769868e6c779ea6fb00c4601569eb2
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-27 11:12:55 -0700

    Updated Notes

--- Assets/Script/GenerateNavMesh.cs
+++ Assets/Script/GenerateNavMesh.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class GenerateNavMesh : MonoBehaviour
 {
+
+    public NavMeshSurface _surface;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Jump"))
         {
+            Debug.Log("Building...?");
+            _surface.BuildNavMesh();
 
         }
     }
